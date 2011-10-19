9352c5024c61d864d9f0bb041a3b56ae18bd75f9
Benjamin Anderson <b@banjiewen.net>
2011-10-18 22:04:14 -0700

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject xmpp-clj "0.1.0"
-  :description "A clojure wrapper around the smack jabber/XMPP library"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [jivesoftware/smack "3.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+(defproject xmpp-clj "0.2.0"
+  :description "A Clojure wrapper around the Smack jabber/XMPP library"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.igniterealtime.smack/smack "3.2.1"]])
