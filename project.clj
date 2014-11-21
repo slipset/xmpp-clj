ca2ddb1294ecd4f75a1acb459f59cce049e7bcb9
Erik Assum <erik@iterate.no>
2014-11-21 15:24:32 +0100

    Converted to midje for testing (use 'midje.repl) (autotest)

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject net.assum/xmpp-clj "0.0.1"
   :description "A lightweight clojure wrapper around the smack jabber/XMPP library. Hipchat compatible."
-   :repl-options {
-                  :init-ns xmpp-clj.bot
-                  }
+   :repl-options {:init-ns xmpp-clj.bot}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [jivesoftware/smack "3.1.0"]
-                 [jivesoftware/smackx "3.1.0"]
-		 ])
+                 [jivesoftware/smackx "3.1.0"]]
+   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+
--- test/xmpp_clj/bot_test.clj
+++ test/xmpp_clj/bot_test.clj
@@ -1,5 +1,6 @@
 (ns xmpp-clj.bot-test
     (:require [clojure.test :refer :all]
+              [midje.sweet  :refer :all]   
               [xmpp-clj.bot :refer :all])
     (:import [org.jivesoftware.smack
             Chat ChatManager ConnectionConfiguration MessageListener
@@ -11,68 +12,69 @@
            [org.jivesoftware.smackx.muc
             MultiUserChat DiscussionHistory InvitationListener]))
 
-(deftest test-create-message
-  (testing "that it works"
+(facts test-create-message
+  (fact "that it works"
     (let [to "myuser"
           type Message$Type/chat
           body "foo"
           message (create-message :from {:from to :type type :response body})]
-      (is (= body (.getBody message)))
-      (is (= type (.getType message)))
-      (is (= to (.getTo message))))))
+      (.getBody message) => body 
+      (.getType message) => type
+      (.getTo message) => to)))
       
-(deftest test-wrap-responder
-  (testing "that sender is called when a message is returned"
-    (let [fn (wrap-responder (fn [conn msg] "foo") (fn [conn msg] msg))]
-      (is (function? fn))
-      (is (= (:response (fn nil {}) "foo")))))
+(facts test-wrap-responder
+       (fact "that sender is called when a message is returned"
+             (let [fn (wrap-responder (fn [conn msg] "foo") (fn [conn msg] msg))]
+               (function? fn) => true
+               (:response (fn nil {})) => "foo"))
   
-  (testing "that sender is not called when a message is not returned"
+  (fact "that sender is not called when a message is not returned"
     (let [fn (wrap-responder (fn [conn msg] nil) #(%1 %2))]
-      (is (function? fn))
-      (is (not (fn nil {}))))))
+      (function? fn) => true
+      (fn nil {}) => nil)))
 
-(deftest test-connection-type
-  (testing "that connection-type returns the correct type"
-    (is (= String (connection-type "s" :a :b :c)))))
+(facts test-connection-type
+       (fact "that connection-type returns the correct type"
+             (connection-type "s" :a :b :c) => String))
 
-(deftest test-cfg->conn
-  (testing "that we're getting the correct stuff back"
-    (is (= (type (cfg->conn {:host "foo" :domain "bar"})) XMPPConnection))))
 
-(deftest test-connect
-  (testing "that an exception is thrown if required params are not present"
-    (is (thrown? Exception (connect {}))))
+(facts test-cfg->conn
+  (fact "that we're getting the correct stuff back"
+    (type (cfg->conn {:host "foo" :domain "bar"})) => XMPPConnection))
 
-  (testing "that the connect function works as expected"
+(facts test-connect
+  (fact"that an exception is thrown if required params are not present"
+    (connect {}) => (throws Exception)) 
+
+  (fact "that the connect function works as expected"
     (with-redefs-fn {#'do-connect (fn [conn credentials] credentials)}
       #(let [cfg {:username "foo"
                   :password "bar"
                   :host "localhost"
                   :domain "localhost"}]
-         (is (= (connect cfg) cfg))))))
+         (connect cfg) => cfg))))
 
-(deftest test-create-sender
-  (testing "that it works"
+(facts test-create-sender
+  (fact "that it works"
     (with-redefs-fn {#'send-message (fn [conn msg] msg)}
       #(let [fun (create-sender identity)]
-         (is (= (fun nil 1) 1))))))
+         (fun nil 1) => 1))))
 
-(deftest test-wrap-remove-connection
-  (testing "that it removes the connection in the handler chain"
+(facts test-wrap-remove-connection
+  (fact "that it removes the connection in the handler chain"
     (let [fun (wrap-remove-connection identity)]
-      (is (= (fun nil 1) 1)))))
+      (fun nil 1) =>  1)))
 
-(deftest test-wrap-remove-message
-  (testing "that wrap-remove-message works"
+(facts test-wrap-remove-message
+  (fact "that wrap-remove-message works"
     (let [fun (wrap-remove-message identity (fn [m] (= m 2)))]
-      (is (= (fun 1) 1))
-      (is (= (fun 2) nil)))))
+      (fun 1) => 1
+      (fun 2) => nil)))
 
-(deftest test-with-message-map
-  (testing "that we get a map from a message"
+(facts test-with-message-map
+  (fact "that we get a map from a message"
     (let [msg (create-message :from {:response "foo" :type Message$Type/chat})
           mapfn (with-message-map (fn [conn msg] msg))
           result (mapfn nil msg)]
-      (is (= (:body result) "foo"))
-      (is (= (:type result) Message$Type/chat)))))
+      (:body result) => "foo"
+      (:type result) => Message$Type/chat)))
