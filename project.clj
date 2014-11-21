b22857f956b4eecfa0af99402e3c9c0de34c884a
Erik Assum <erik@iterate.no>
2014-11-21 22:13:09 +0100

    Added :scm tag

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject net.assum/xmpp-clj "0.0.1"
+(defproject net.assum/xmpp-clj "0.0.2"
   :description "A lightweight clojure wrapper around the smack jabber/XMPP library. Hipchat compatible."
    :repl-options {:init-ns xmpp-clj.bot}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-		 [jivesoftware/smack "3.1.0"]
-                 [jivesoftware/smackx "3.1.0"]]
-   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+		 [org.igniterealtime.smack/smack "3.2.1"]
+                 [org.igniterealtime.smack/smackx "3.2.1"]]
+   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+    :scm {:name "git"
+          :url "https://github.com/slipset/xmpp-clj.git"})
 
